b26d4abcd7757c1ac6562243a04c03e762698fd6
saimter <saimter@naver.com>
2025-02-03 12:31:03 +0900

    반복문 추가

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Git Test");
             Console.WriteLine("캠프에서 추가");
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine(i);
+            }
         }
     }
 }
+
