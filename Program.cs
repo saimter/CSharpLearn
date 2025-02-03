4bf4896dfca50e21524832c2d8e012e134085ae6
saimter <saimter@naver.com>
2025-02-03 14:43:44 +0900

    2차원 배열로 1부터 100까지 입력 후 출력

--- Program.cs
+++ Program.cs
@@ -34,9 +34,10 @@
                 Console.WriteLine("");
             }
             Console.WriteLine("--------------------별과 샾 그리기------------------");
-            for (int i = 0; i < 5; i++)
+            int size = 10;
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 5; j >= 0; j--)
+                for (int j = size; j >= 0; j--)
                 {
                     if (i >= j)
                     {
@@ -49,26 +50,37 @@
                 }
                 Console.WriteLine("");
             }
-            Console.WriteLine("---------------2차원 배열-----------------------");
-            int[,] map = new int[5,5];
-            int size = 10;
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("---------------배열-----------------------");
+            size = 10;
+            int[] data = new int[size];
+
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < 5; j++)
-                {
-                    map[j, i] = i;
-                    //if (i >= j)
-                    //{
-                    //    Console.Write("*");
-                    //}
-                    //else
-                    //{
-                    //    Console.Write("#");
-                    //}
-                }
-                Console.WriteLine("");
+                data[i] = i + 1;
+            }
+            for (int i = 0; i < size; i++) {
+                Console.WriteLine(data[i]);
             }
 
+            String s = "Hello World!!!";
+
+            int[,] data2 = new int[10,10];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    data2[i, j] = i * 10 + j + 1;
+                }
+            }
+            Console.WriteLine("-------------- 2차원 배열------------------------");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write($" {data2[i,j]} \t");
+                }
+                Console.WriteLine($" ");
+            }
             Console.WriteLine("--------------------------------------");
         }
     }
