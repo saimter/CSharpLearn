d0e8a7d270f114ab52b816f94eca7591ec5c8621
saimter <saimter@naver.com>
2025-02-03 13:36:08 +0900

    오른쪽 삼각형 그리기

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Git Test");
             Console.WriteLine("캠프에서 추가");
+
+            Console.WriteLine("------------------별 그리기--------------------");
             for (int i = 0; i < 5; i++) 
             {
                 for (int j = 0; j < 5; j++)
@@ -15,6 +17,59 @@
                 }
                 Console.WriteLine("");
             }
+
+            Console.WriteLine("--------------------별과 샾 그리기------------------");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if( i >= j) {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("--------------------별과 샾 그리기------------------");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 5; j >= 0; j--)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("---------------2차원 배열-----------------------");
+            int[,] map = new int[5,5];
+            int size = 10;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    map[j, i] = i;
+                    //if (i >= j)
+                    //{
+                    //    Console.Write("*");
+                    //}
+                    //else
+                    //{
+                    //    Console.Write("#");
+                    //}
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("--------------------------------------");
         }
     }
 }
